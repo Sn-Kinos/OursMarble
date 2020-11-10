be96c267fa0831540ccee05bde480edf0ca0fa27
Sn-Kinos <dony435@naver.com>
2020-11-10 11:52:04 +0900

    make test class

--- OursMarble/Form1.cs
+++ OursMarble/Form1.cs
@@ -17,5 +17,6 @@ namespace OursMarble
             InitializeComponent();
         }
 
+
     }
 }
---
+++ OursMarble/Land.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OursMarble
+{
+    class Land
+    {
+        public Land(int basePrice, Land prev, Land next)
+        {
+            
+        }
+    }
+}
